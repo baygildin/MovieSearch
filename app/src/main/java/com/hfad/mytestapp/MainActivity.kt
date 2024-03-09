4fcc11d2f8af41ac8ed0a4bd0ce5c64144fa823f
baygildin <baigildin.samat@gmail.com>
2024-03-09 19:01:00 +0500

    Создал новую ветку

--- app/src/main/java/com/hfad/mytestapp/MainActivity.kt
+++ app/src/main/java/com/hfad/mytestapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.hfad.mytestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//ssdsd
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

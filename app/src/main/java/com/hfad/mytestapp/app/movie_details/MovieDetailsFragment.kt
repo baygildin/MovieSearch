a2a9832973577f7f05edde75e40abb9cc4117008
baygildin <baigildin.samat@gmail.com>
2024-04-03 12:52:54 +0500

    Seventh commit: added navigation for 3 screens

--- app/src/main/java/com/hfad/mytestapp/app/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/com/hfad/mytestapp/app/movie_details/MovieDetailsFragment.kt
@@ -10,11 +10,15 @@ import android.widget.Button
 import androidx.navigation.findNavController
 
 class MovieDetailsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_movie_details, container, false)
+        val startButton = view.findViewById<Button>(R.id.poster_to_maxSize_button)
+        startButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_movieDetailsFragment_to_posterFragment)
+
+        }
+        return view
     }
 }

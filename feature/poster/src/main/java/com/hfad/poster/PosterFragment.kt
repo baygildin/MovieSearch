c053656d92994034a1846e86ee91fd93dadd3b68
baygildin <baigildin.samat@gmail.com>
2024-05-05 23:29:56 +0500

    Прикрутил фрагментам feature по viewBinding`у, удалил лишине тестовые файлы

--- feature/poster/src/main/java/com/hfad/poster/PosterFragment.kt
+++ feature/poster/src/main/java/com/hfad/poster/PosterFragment.kt
@@ -6,25 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.hfad.poster.databinding.FragmentPosterBinding
 
 class PosterFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = PosterFragment()
-    }
+    private var _binding: FragmentPosterBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding in FragmentPosterBinding of FragmentPoster must not be null")
+
 
     private val viewModel: PosterViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_poster, container, false)
+        _binding = FragmentPosterBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
     }
 }
--- feature/show_episodes/src/main/java/com/hfad/show_episodes/ShowEpisodesFragment.kt
+++ feature/show_episodes/src/main/java/com/hfad/show_episodes/ShowEpisodesFragment.kt
@@ -4,28 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.fragment.findNavController
-
+import com.hfad.show_episodes.databinding.FragmentShowEpisodesBinding
 
 
 class ShowEpisodesFragment : Fragment() {
+    private var _binding: FragmentShowEpisodesBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding in FragmentShowEpisodesBinding of FragmentShowEpisodes Fragment must not be null")
+
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_show_episodes, container, false)
-        val moveToDetailsScreenButton = view.findViewById<Button>(R.id.btn_details_screen)
-
-        moveToDetailsScreenButton.setOnClickListener {
+//        val view = inflater.inflate(R.layout.fragment_show_episodes, container, false)
+        _binding = FragmentShowEpisodesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        binding.btnDetailsScreen.setOnClickListener {
             val request = NavDeepLinkRequest.Builder
                 .fromUri("android-app://com.hfad.movie_details/movieDetailsFragment".toUri())
                 .build()
             findNavController().navigate(request)
         }
+
         return view
 
     }

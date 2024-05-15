751353b4a588be4fb9933a538d852fc3e00e13aa
baygildin <baigildin.samat@gmail.com>
2024-05-15 20:20:50 +0500

    Отправляю на проверку. Добавил Splash screen. Реализовал кнопку "Избранное" с помощью Room. Удалил лишний пакет и файлы. Добавил кнопки, открывает экран показа серий, и информации о них. Room не смог подключить пока, вместо него просто заглушка. Получилось отправить строку только через SharedViewModel. Добавил вывод детальной информации о фильме. Добавил показ постера в Movie_details. Добавил просмотр полнорамзерного постера.Поменял поиск на "по названию". Вывел списком все ответы. Дизайн по фигме начал делать Научился вызывать инфу про запрос по кнопке "получить инфу во фрагменте" Пробую отправить в другой фрагмент строку Прикрутил фрагментам feature по viewBinding`у, удалил лишние тестовые файлы.

--- app/src/main/java/com/hfad/mytestapp/MainActivity.kt
+++ app/src/main/java/com/hfad/mytestapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hfad.mytestapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,10 +10,8 @@ class MainActivity : AppCompatActivity() {
     private val SPLASH_DELAY: Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
+
     }
 }
-
-
-
-

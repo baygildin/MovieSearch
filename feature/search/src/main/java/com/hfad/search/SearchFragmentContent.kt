5c050cf8016a100c7ddcc770b24fb9f956284fd7
baygildin <baigildin.samat@gmail.com>
2024-09-11 02:01:16 +0500

    made localization of login fragment and added email check by Regex

--- feature/search/src/main/java/com/hfad/search/SearchFragmentContent.kt
+++ feature/search/src/main/java/com/hfad/search/SearchFragmentContent.kt
@@ -1,5 +1,7 @@
 package com.hfad.search
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -24,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -41,6 +44,7 @@ fun SearchFragmentContent(searchViewModel: SearchViewModel, navigateToDetails: (
     val searchResults = searchViewModel.searchResults.collectAsState().value
     var searchJob by remember { mutableStateOf<Job?>(null) }
     val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
 
     MaterialTheme(
         colors = MaterialTheme.colors.copy(
@@ -69,12 +73,17 @@ fun SearchFragmentContent(searchViewModel: SearchViewModel, navigateToDetails: (
             TextField(
                 value = query.value,
                 onValueChange = { newQuery ->
-                    query.value = newQuery
-                    searchJob?.cancel()
-                    searchJob = coroutineScope.launch {
-                        delay(700)
-                        searchViewModel.searchMediaWithTitle(newQuery)
+                    if (newQuery.matches(Regex("^[a-zA-Z0-9]*$"))) {
+                        query.value = newQuery
+                        searchJob?.cancel()
+                        searchJob = coroutineScope.launch {
+                            delay(700)
+                            searchViewModel.searchMediaWithTitle(newQuery)
+                        }
+                    } else {
+                        showToast(context, context.getString(R.string.toast_only_eng_chars))
                     }
+
                 },
                 placeholder = {
                     Text(
@@ -117,4 +126,8 @@ fun SearchFragmentContent(searchViewModel: SearchViewModel, navigateToDetails: (
         }
 
     }
+}
+
+fun showToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
